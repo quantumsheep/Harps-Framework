d48f4a13811c52be426452748877ff30ec820940
QuantumSheep <quantumsheep@outlook.fr>
2017-09-10 23:39:48 +0200

    Create a skeleton for Harps

--- App/Models/DefaultModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-
-class DefaultModel
-{
-    public $php_version;
-    public $current_uri;
-}
--- Harps/Boot.php
+++ Harps/Boot.php
@@ -11,15 +11,12 @@ class Boot
             session_start();
         }
 
-        require_once(dirname(__DIR__) . "/Config/Parameters.php");
-		
-		if(!file_exists(DIR_BLADE_CACHE)) {
-			Directories::create(DIR_BLADE_CACHE);
-		}
-		
-        require_once(DIR_HARPS . "Helpers.php");
         Harps\Core\Handler::register();
 
+        if(!file_exists(DIR_BLADE_CACHE)) {
+			Directories::create(DIR_BLADE_CACHE);
+        }
+        
         require_once(DIR_CONFIG . "Routes.php");
 
         if(isset($GLOBALS["ROUTED"]) && $GLOBALS["ROUTED"] != true) {
